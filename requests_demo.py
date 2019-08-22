be840979f2ed40309b69ea00a63d86d9363a6f93
lxherman <549622112@qq.com>
2019-08-22 11:16:25 +0800

    requests测试模板

---
+++ requests_demo.py
@@ -0,0 +1,23 @@
+import requests
+import json
+import random
+
+def rand_ua():
+    rand_ua_j = random.randint(0, 99)
+    rand_ua_i = random.randint(100, 999)
+    ua = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.%s.3770.%s Safari/537.36" % (
+        rand_ua_j, rand_ua_i)
+    return ua
+
+api = 'http://www.winshangdata.com/api/ProjectMapApi.aspx'
+headers = {
+    'User-Agent': rand_ua(),
+}
+
+data = {
+    "":"",
+    "":""
+}
+
+# re = requests.get(api,headers=headers)
+re = requests.post(api,headers=headers,data=data)
